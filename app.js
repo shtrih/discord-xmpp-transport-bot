7725d488f409c6db404b63fd073e6a766b4e4bbe
shtrih <shtrih@hitagi.ru>
2016-07-03 05:22:00 +0400

    Cleaning code, implemented presence handlers

--- app.js
+++ app.js
@@ -8,126 +8,200 @@
 
 var Discord = require('discord.io');
 var Xmpp = require('node-xmpp-client');
-var util = require('util');
-var Config = require('json-config')({
-    config_dir: "./",
-    default_env: ".app"
-});
+var Util = require('util');
+var Config = require('json-config');
 
-var discord = new Discord.Client({
-        token: Config.discord.token,
-        autorun: true
-    }),
-    jabber = new Xmpp.Client({
-        jid: Config.jabber.userJid,
-        password: Config.jabber.userPass
-    })
-;
+/*
+var stanza = Xmpp.createStanza('message', {from: 'test', to: 'asda', type: 'asd'}, new Xmpp.Element('body').t('>asdasdas'));
+console.log(stanza.toString(), stanza.from, stanza.foo, stanza.attrs.foo);
+console.log(Xmpp.createStanza('message', {from: 'test', to: 'asda', type: 'asd'}).c('body').t('>asdasdas').up().toString());
+console.log((new Xmpp.Element('message', { to: 'asd', type: 'groupchat' })).toString());
+*/
 
-discord.on('ready', function () {
-    console.log(discord.username + " - (" + discord.id + ")");
-});
+new App().run();
 
-discord.on('message', function (user, userID, channelID, message, event) {
-    if (message === "ping") {
-        discord.sendMessage({
-            to: channelID,
-            message: "pong"
-        });
-    }
-    else if (Config.discord.channelId == channelID) {
-        if (!message.match(/^>.+:/)) {
-            jabber.send(new Xmpp.Element('message', { to: Config.jabber.roomJid, type: 'groupchat' }).
-                c('body').t('>' + user + ': ' + message)
-            );
-        }
-    }
-});
-
-
-jabber.on('online', function () {
-    console.log('We are online!');
-    //var elm2 = new xmpp.Element('presence', { from: jid, to: 'alfred@conference.localhost'}).c('x', {'xmlns': 'http://jabber.org/protocol/muc' }).up();
-
-    // set ourselves as online
-    /*jabber.send(new xmpp.Element('presence', { type: 'available' }).
-     c('show').t('chat')
-     );*/
-
-    jabber.send(new Xmpp.Element('presence', { to: Config.jabber.roomJid + '/' + Config.jabber.roomNick }).
-        c('x', { xmlns: 'http://jabber.org/protocol/muc' })
-    );
-
-    /*jabber.send(new xmpp.Element('message', { to: Config.jabber.roomJid, type: 'groupchat' }).
-     c('body').t('test')
-     );*/
-});
-
-jabber.on('error', function (e) {
-    util.log(e);
-});
-
-jabber.on('connection', function () {
-    util.log('online');
-});
-
-jabber.on('stanza', function (stanza) {
-    util.log('Incoming stanza: ', stanza.toString());
-
-    // always log error stanzas
-    if ('error' == stanza.attrs.type) {
-        util.log('[error] ' + stanza);
-        return;
-    }
-
-    // ignore everything that isn't a room message
-    if (!stanza.is('message') || !stanza.attrs.type == 'groupchat') {
-        return;
-    }
-
-    // ignore messages we sent
-    if (stanza.attrs.from == Config.jabber.roomJid + '/' + Config.jabber.roomNick) {
-        return;
-    }
-
-    /**
-     * TODO: Переименовался
-     * Incoming stanza:  <presence from="dumb@conference.hitagi.ru/shtrih" to="senjougahara-hitagi@jabber.ru/2438158504" type="unavailable" xmlns:stream="http://etherx.jabber.org/streams"><x xmlns="http://jabber.org/protocol/muc#user"><item jid="crab@hitagi.ru/su" affi
-     liation="none" role="participant" nick="asd"/><status code="303"/></x></presence>
-     Incoming stanza:  <presence from="dumb@conference.hitagi.ru/asd" to="senjougahara-hitagi@jabber.ru/2438158504" xml:lang="ru" xmlns:stream="http://etherx.jabber.org/streams"><x xmlns="vcard-temp:x:update"><photo/></x><priority>25</priority><c xmlns="http://jabber
-     .org/protocol/caps" node="http://qip.ru/caps" ver="6848" ext="voice-v1 webcam-v1"/><x xmlns="http://jabber.org/protocol/muc#user"><item jid="crab@hitagi.ru/su" affiliation="none" role="participant"/></x></presence>
-     * Зашел:
-     * <presence from="dumb@conference.hitagi.ru/alien.army.knife" to="senjougahara-hitagi@jabber.ru/3945639340" xmlns:stream="http://etherx.jabber.org/streams"><priority>1</priority><c xmlns="http://jabber.org/protocol/caps" node="http://pidgin.im/"
-     hash="sha-1" ver="s/y3ONmuAkM0tFGeXowWeZc6/Hc="/><x xmlns="vcard-temp:x:update"><photo/></x><x xmlns="http://jabber.org/protocol/muc#user"><item jid="alien.army.knife@jabber.ru/IDSE" affiliation="none" role="participant"/></x></presence>
-     * Вышел:
-     *
-     */
-
-    var body = stanza.getChild('body'),
-        subject = stanza.getChild('subject')
-    ;
-    if (subject) {
-        var topic = (body ? body : subject).getText();
-        util.log('Try to set discord topic: ', topic);
-        discord.editChannelInfo({
-            channel: Config.discord.channelId,
-            topic: topic
-        });
-    }
-
-    // message without body is probably a topic change
-    if (!body) {
-        return;
-    }
-
-    var message = body.getText(),
-        from_nick = stanza.attrs.from.split('/', 2)[1]
+function App() {
+    var self = this,
+        discord,
+        jabber,
+        config = Config({
+            config_dir: "./",
+            default_env: ".app"
+        }),
+        jid_by_channel = {},
+        channel_by_jid = {},
+        nick_by_jid = {},
+        jabber_connected_users = {}
     ;
 
-    if (!message.match(/^>.+:/)) {
-        discord.sendMessage({
-            to: Config.discord.channelId,
-            message: '>' + from_nick + ': ' + message
+    this.run = function () {
+        discord = new Discord.Client({
+                token: config.discord.token,
+                autorun: true
         });
-    }
-});
+        jabber = new Xmpp.Client({
+            jid: config.jabber.userJid,
+            password: config.jabber.userPass
+        });
+
+        discord.on('ready', function () {
+            Util.log('Connected to discord as ' + discord.username + " - (" + discord.id + ")");
+
+
+        });
+
+        discord.on('message', function (fromNickname, userID, channelID, message, event) {
+            Util.log(jabber_connected_users, jid_by_channel);
+            if ("ping" === message) {
+                discord.sendMessage({
+                    to: channelID,
+                    message: "pong"
+                });
+            }
+            else if ("users" === message) {
+                if ("object" === typeof(jabber_connected_users[ jid_by_channel[channelID] ]))
+                    discord.sendMessage({
+                        to: channelID,
+                        message: 'Участники: ' + Object.keys(jabber_connected_users[ jid_by_channel[channelID] ]).join(', ')
+                    });
+            }
+            else if (userID != discord.id) {
+                // https://discordapp.com/developers/docs/resources/channel#message-formatting
+                message = discord.fixMessage(message);
+
+                jabber.send(
+                    Xmpp.createStanza('message', {to: jid_by_channel[channelID], type: 'groupchat'}, new Xmpp.Element('body').t('>*' + fromNickname + '*: ' + message))
+                );
+            }
+        });
+
+        jabber.on('online', function () {
+            Util.log('Connected to jabber as ' + config.jabber.userJid);
+
+            // set ourselves as online
+            /*jabber.send(new xmpp.Element('presence', { type: 'available' }).
+             c('show').t('chat')
+             );*/
+
+            for (var i = 0 ; i < config.roomList.length; i++) {
+                self.join(config.roomList[i].roomJid, config.roomList[i].nick);
+
+                jid_by_channel[ config.roomList[i].roomChannelId ] = config.roomList[i].roomJid;
+                channel_by_jid[ config.roomList[i].roomJid ] = config.roomList[i].roomChannelId;
+                nick_by_jid[ config.roomList[i].roomJid ] = config.roomList[i].nick;
+            }
+        });
+
+        jabber.on('error', function (e) {
+            Util.log(e);
+        });
+
+        jabber.on('connection', function () {
+            Util.log('online');
+        });
+
+        jabber.on('stanza', function (stanza) {
+            Util.log('Incoming stanza: ', stanza.toString());
+
+            var from = stanza.from.split('/', 2),
+                from_jid = from[0],
+                from_nick = from[1],
+                channel = channel_by_jid[from_jid],
+                message,
+                use_nick
+            ;
+
+            if ('error' == stanza.type) {
+                Util.log('[error] ' + stanza);
+                return;
+            }
+
+            switch (stanza.getName()) {
+                case 'presence': {
+                    if ('unavailable' === stanza.type) {
+                        // change nick
+                        var x = stanza.getChildByAttr('code', '303', null, true),
+                            item = stanza.getChildrenByFilter(
+                                function (el) {
+                                    return el instanceof Xmpp.Element && el.getName() === 'item';
+                                },
+                                true
+                            )[0]
+                        ;
+
+                        if (x && item) {
+                            var new_nick = item.getAttr('nick');
+                            jabber_connected_users[from_jid][new_nick] = true;
+                            message = from_nick + ' переименовался в ' + new_nick + '.';
+                        }
+                        else {
+                            message = from_nick + ' отключился.'
+                        }
+
+                        delete jabber_connected_users[from_jid][from_nick];
+                    }
+                    else {
+                        if (!jabber_connected_users[from_jid])
+                            jabber_connected_users[from_jid] = {};
+
+                        if (!jabber_connected_users[from_jid][from_nick]) {
+                            message = from_nick + ' подключился.';
+                        }
+
+                        jabber_connected_users[from_jid][from_nick] = true;
+                    }
+
+                    // formatting
+                    if (message)
+                        message = '*'+ message +'*';
+                }
+                break;
+
+                case 'message': {
+                    use_nick = true;
+                    var body = stanza.getChild('body');
+                    if ('groupchat' === stanza.type) {
+                        if (stanza.from == from_jid + '/' + nick_by_jid[from_jid]) {
+                            break;
+                        }
+
+                        var subject = stanza.getChild('subject');
+                        if (subject) {
+                            var topic = (body ? body : subject).getText();
+                            Util.log('Try to set discord topic: ', topic);
+                            discord.editChannelInfo({
+                                channel: channel,
+                                topic: topic + '\n ~ ' + from_jid
+                            });
+
+                            break;
+                        }
+
+                        message = body.getText();
+                    }
+                    else if ('chat' === stanza.type) {
+                        //
+                    }
+                }
+                break;
+
+                case 'iq':
+                break;
+            }
+
+            if (message && !message.match(/^>.+:/)) {
+                discord.sendMessage({
+                    to: channel,
+                    message: (use_nick ? '>**' + from_nick + '**: ' : '') + message
+                });
+            }
+        });
+    };
+
+    this.join = function(JID, nickname) {
+        jabber.send(
+            Xmpp.createStanza('presence', {to: JID + '/' + nickname}, new Xmpp.Element('x', { xmlns: 'http://jabber.org/protocol/muc' }))
+        );
+    };
+}
+
