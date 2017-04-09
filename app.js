2e733d7dd9cbfcc5255c7e11b036077452924809
shtrih <shtrih@hitagi.ru>
2017-04-09 06:01:23 +0400

    Refactoring :recycle:: es6, move xmpp logic to Ram

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const Discord = require('./lib/RemDiscord.js'),
     config = require('json-config')({
         config_dir: "./"
     }),
-    Ignore = require('./lib/ignoreUsers.js')()
+    List = require('./lib/List.js')
 ;
 
 new App().run();
@@ -33,9 +33,11 @@ function App() {
         remDiscord = new Discord(config.discord.token, true),
         discord = remDiscord.getClient(),
         ramXmpp = new Xmpp(config.jabber.userJid, config.jabber.userPass),
-        jabber = ramXmpp.getClient()
+        jabber = ramXmpp.getClient(),
+        Ignore = new List()
     ;
-    let conferenceSendPresenceInterval,
+    let
+        conferenceSendPresenceInterval,
         jid_by_channel = {},
         channel_by_jid = {},
         nick_by_jid = {},
@@ -44,7 +46,7 @@ function App() {
         jabber_connected_users = {}
     ;
 
-    this.run = function () {
+    this.run = () => {
         discord.on('ready', () => {
             LogInfo('Connected to discord as ' + discord.username + " - (" + discord.id + ")");
         });
@@ -63,14 +65,10 @@ function App() {
         discord.on('debug', remDiscord.logDebug);
 
         discord.on('message', (fromNickname, userID, channelID, message, event) => {
-//            LogDebug('jabber_connected_users', jabber_connected_users);
             let match;
 
             if ("!ping" === message) {
-                remDiscord.send(
-                    channelID,
-                    "pong"
-                );
+                remDiscord.send(channelID, "pong");
             }
             else if ("!users" === message) {
                 let reply = 'Не получила сведения о присутствии. Попробуйте повторить завтра.';
@@ -82,10 +80,7 @@ function App() {
                 if (ignored)
                     reply += '\n**В игноре:** ' + ignored;
 
-                remDiscord.send(
-                    channelID,
-                    reply
-                );
+                remDiscord.send(channelID, reply);
             }
             else if (match = message.match(/^!((un)?ignore)\s+(.*)/i)) {
                 const nickname = match[3],
@@ -102,7 +97,7 @@ function App() {
 
                 remDiscord.send(
                     channelID,
-                    '*'+ nickname +'* '+ prefix +'ignored.'
+                    `*${nickname}* ${prefix}ignored.`
                 );
             }
             else if (userID !== discord.id && jid_by_channel[channelID]) {
@@ -170,154 +165,111 @@ function App() {
             LogInfo('Jabber online');
         });
 
-        jabber.on('stanza', function (stanza) {
-            // LogDebugJabber('Incoming: ', stanza.toString());
+        ramXmpp.on('stanza:error', (stanza, from_jid) => {
+            remDiscord.send(
+                this.getChannelByJid(from_jid),
+                '**[stanza:error]** ```' + stanza + '```'
+            );
+        });
 
-            let from = stanza.from.split('/', 2),
-                from_jid = from[0],
-                from_nick = from[1],
-                channel = channel_by_jid[from_jid],
-                message,
-                use_nick
-            ;
+        ramXmpp.on('presence:connect', (stanza, from_jid, from_nick) => {
+            if (!jabber_connected_users[from_jid])
+                jabber_connected_users[from_jid] = {};
 
-            if ('error' === stanza.type) {
-                LogErrorJabber('[Stanza error] ' + stanza);
+            if (!jabber_connected_users[from_jid][from_nick]) {
+                jabber_connected_users[from_jid][from_nick] = true;
+
+                if (!this.needShowPresence(from_jid)) {
+                    return;
+                }
 
                 remDiscord.send(
-                    channel,
-                    '**[Stanza error]** ```' + stanza + '```'
-                );
-
-                return;
-            }
-
-            switch (stanza.getName()) {
-                case 'presence': {
-                    if ('unavailable' === stanza.type) {
-                        // change nick
-                        let x = stanza.getChildByAttr('code', '303', null, true),
-                            item = stanza.getChildrenByFilter(
-                                function (el) {
-                                    return el instanceof jabber.Element && el.getName() === 'item';
-                                },
-                                true
-                            )[0]
-                        ;
-
-                        if (x && item) {
-                            const new_nick = item.getAttr('nick');
-                            jabber_connected_users[from_jid][new_nick] = true;
-                            message = from_nick + ' переименовался в ' + new_nick + '.';
-                        }
-                        else {
-                            message = from_nick + ' отключился.'
-                        }
-
-                        delete jabber_connected_users[from_jid][from_nick];
-                    }
-                    else {
-                        if (!jabber_connected_users[from_jid])
-                            jabber_connected_users[from_jid] = {};
-
-                        if (!jabber_connected_users[from_jid][from_nick]) {
-                            message = from_nick + ' подключился.';
-                        }
-
-                        jabber_connected_users[from_jid][from_nick] = true;
-                    }
-
-                    // formatting
-                    if (message)
-                        message = '*'+ message +'*';
-
-                    if (typeof show_presence_by_jid[from_jid] === 'boolean') {
-                        if (!show_presence_by_jid[from_jid])
-                            message = '';
-                    }
-                    else if (!config.jabber.showPresence) {
-                        message = '';
-                    }
-                }
-                break;
-
-                case 'message': {
-                    // ours messages from the discord
-                    if (from_nick === nick_by_jid[from_jid])
-                        return;
-
-                    use_nick = true;
-                    let body = stanza.getChild('body'),
-                        x = stanza.getChild('x'),
-                        delay = stanza.getChild('delay'),
-                        subject = stanza.getChild('subject')
-                    ;
-
-                    /* Skip chat history
-                     *
-                     * <message from="dumb@conference.hitagi.ru/crab" to="senjougahara-hitagi@jabber.ru/1502680524" type="groupchat" id="purple97826163" xmlns:stream="http://etherx.jabber.org/streams">
-                     *     <body>123</body>
-                     *     <x xmlns="jabber:x:delay" stamp="20160708T09:43:37"/>
-                     * </message>
-                     *
-                     * <message from="animufags@conference.jabber.ru/Инопланетная бaка" to="senjougahara-hitagi@jabber.ru/1035180042" xml:lang="ru" type="groupchat" id="ab55a" xmlns:stream="http://etherx.jabber.org/streams">
-                     *     <body>он вроде просто писал любое сообщение и она глючила</body>
-                     *     <delay xmlns="urn:xmpp:delay" from="animufags@conference.jabber.ru" stamp="2016-07-13T19:46:31.799Z"/>
-                     * </message>
-                     */
-                    if (!subject && (delay || x && 'jabber:x:delay' === x.getAttr('xmlns'))) {
-                        break;
-                    }
-
-                    /* Server messages
-                     *
-                     *  <message from='dumb@conference.hitagi.ru' to='senjougahara-hitagi@jabber.ru/52626594' type='groupchat'>
-                     *      <body>This room is not anonymous</body>
-                     *      <x xmlns='http://jabber.org/protocol/muc#user'><status code='100'/></x>
-                     *  </message>
-                     */
-                    if (!from_nick)
-                        from_nick = 'Server';
-
-                    if ('groupchat' === stanza.type) {
-                        if (stanza.from === from_jid + '/' + nick_by_jid[from_jid]) {
-                            break;
-                        }
-
-                        if (Ignore.check(from_nick)) {
-                            LogDebug('Ignore msg from ' + from_nick);
-                            break;
-                        }
-
-                        if (subject) {
-                            const topic = (body ? body : subject).getText();
-                            LogDebugJabber('Trying to set discord topic: ', topic);
-                            remDiscord.editChannel(channel, topic + '\n ~ ' + from_jid);
-
-                            break;
-                        }
-
-                        if (body) {
-                            message = body.getText();
-                        }
-                    }
-                    else if ('chat' === stanza.type) {
-                        // direct messages
-                    }
-                }
-                break;
-
-                case 'iq':
-                break;
-            }
-
-            if (message) {
-                remDiscord.send(
-                    channel,
-                    (use_nick ? '**' + from_nick + '**: ' : '') + message
+                    this.getChannelByJid(from_jid),
+                    `*${from_nick} подключился.*`
                 );
             }
         });
+
+        ramXmpp.on('presence:disconnect', (stanza, from_jid, from_nick, Status) => {
+            delete jabber_connected_users[from_jid][from_nick];
+
+            if (!this.needShowPresence(from_jid)) {
+                return;
+            }
+
+            if (!Status || '303' !== Status.getAttr('code')) {
+                remDiscord.send(
+                    this.getChannelByJid(from_jid),
+                    `*${from_nick} отключился.*`
+                );
+            }
+        });
+
+        ramXmpp.on('presence:disconnect:kick', (stanza, from_jid, from_nick, Actor, Reason) => {
+            const byNick = Actor ? Actor.getAttr('nick') : '',
+                reason = Reason ? Reason.getText() : 'причина не указана'
+            ;
+            remDiscord.send(
+                this.getChannelByJid(from_jid),
+                `*${from_nick} выгнан (${byNick}: ${reason}).*`
+            );
+        });
+
+        ramXmpp.on('presence:disconnect:ban', (stanza, from_jid, from_nick, Actor, Reason) => {
+            const byNick = Actor ? Actor.getAttr('nick') : '',
+                reason = Reason ? Reason.getText() : 'причина не указана'
+            ;
+            remDiscord.send(
+                this.getChannelByJid(from_jid),
+                `*${from_nick} забанен (${byNick}: ${reason}).*`
+            );
+        });
+
+        ramXmpp.on('presence:rename', (stanza, from_jid, from_nick, new_nick) => {
+            delete jabber_connected_users[from_jid][from_nick];
+            jabber_connected_users[from_jid][new_nick] = true;
+
+            remDiscord.send(
+                this.getChannelByJid(from_jid),
+                `*${from_nick} переименовался в ${new_nick}.*`
+            );
+        });
+
+        ramXmpp.on('message:groupchat:subject', (stanza, from_jid, from_nick, Subject, has_delay) => {
+            const topic = Subject.getText();
+            LogInfo('Trying to set discord topic: ', topic);
+            remDiscord.editChannel(this.getChannelByJid(from_jid), topic + '\n ~ ' + from_jid);
+        });
+
+        ramXmpp.on('message:groupchat', (stanza, from_jid, from_nick, Body, has_delay) => {
+            if (has_delay) {
+                return;
+            }
+            if (from_nick === nick_by_jid[from_jid]) {
+                return;
+            }
+            if (Ignore.check(from_nick)) {
+                LogDebug('Ignore msg from ' + from_nick);
+                return;
+            }
+
+            remDiscord.send(
+                this.getChannelByJid(from_jid),
+                '**' + from_nick + '**: ' + Body.getText()
+            );
+        });
+    };
+
+    this.getChannelByJid = (conferenceJID) => {
+        return channel_by_jid[conferenceJID];
+    };
+
+    this.needShowPresence = (conferenceJID) => {
+        if (typeof show_presence_by_jid[conferenceJID] === 'boolean') {
+            return show_presence_by_jid[conferenceJID]
+        }
+
+        return config.jabber.showPresence
     };
 
     this.getNicknameWMask = function (roomJid, fromNick) {
